d9bb7a9ef3ad5a4dd9fbd6939919e24b492556f9
NxtaSunstrike <unixalle765@mail.ru>
2024-10-09 07:19:29 -0400

    rename confirmation ocdes to otp

--- src/app/infrastructure/authentification/interfaces/otp_codes_storage.py
+++ src/app/infrastructure/authentification/interfaces/otp_codes_storage.py
@@ -4,21 +4,21 @@ from abc import abstractmethod
 from src.app.domain.user.value_objects import UserUUID
 
 
-class UserConfirmationCodesInterface(Protocol):
+class UserOTPCodesInterface(Protocol):
     @abstractmethod
-    async def get_user_confirmation_code(self: Self, user_id: UserUUID) -> int:
+    async def get_user_otp_code(self: Self, user_id: UserUUID) -> int:
         raise NotImplementedError(
             'method must be implemented by subclasses'
         )
     
     @abstractmethod
-    async def add_user_confiramation_code(self: Self, user_id: UserUUID, code: int) -> None:
+    async def add_user_otp_code(self: Self, user_id: UserUUID, code: int) -> None:
         raise NotImplementedError(
             'method must be implemented by subclasses'
         )
     
     @abstractmethod
-    async def remove_user_confirmation_code(self: Self, user_id: UserUUID) -> None:
+    async def remove_user_otp_code(self: Self, user_id: UserUUID) -> None:
         raise NotImplementedError(
             'method must be implemented by subclasses'
         )
