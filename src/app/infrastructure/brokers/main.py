c43360c7d99223aeae60101a39b87aeecce9064f
NxtaSunstrike <unixalle765@mail.ru>
2024-10-10 14:54:51 -0400

    edit main in brokers

--- src/app/infrastructure/brokers/main.py
+++ src/app/infrastructure/brokers/main.py
@@ -8,6 +8,6 @@ from src.app.infrastructure.brokers.factories import ConnectionFactory
 async def build_nats_connection(
     nats_config: NatsConfig
 ) -> AsyncGenerator[Client, None]:
-    nats_connection_pool = ConnectionFactory(config=nats_config).get_connection()
+    nats_connection_pool: Client = await ConnectionFactory(config=nats_config).get_connection()
     async with nats_connection_pool as nats_connection:
         yield nats_connection
