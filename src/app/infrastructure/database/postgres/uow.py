69dd17eaac9af697e71b75e2e9fc7684801c4135
NxtaSunstrike <unixalle765@mail.ru>
2024-10-11 08:17:10 -0400

    annotatations in infrastrucutre

--- src/app/infrastructure/database/postgres/uow.py
+++ src/app/infrastructure/database/postgres/uow.py
@@ -1,3 +1,4 @@
+from typing import Self
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -6,16 +7,16 @@ from src.app.application.interfaces.uow import UnitOfWork
 
 
 class SqlaUnitOfWork(UnitOfWork):
-    def __init__(self, session: AsyncSession) -> None:
+    def __init__(self: Self, session: AsyncSession) -> None:
         self.session: AsyncSession = session
 
-    async def commit(self) -> None:
+    async def commit(self: Self) -> None:
         try:
             await self.session.commit()
         except SQLAlchemyError:
             raise UoWCommitError
 
-    async def rollback(self) -> None:
+    async def rollback(self: Self) -> None:
         try:
             await self.session.rollback()
         except SQLAlchemyError:
