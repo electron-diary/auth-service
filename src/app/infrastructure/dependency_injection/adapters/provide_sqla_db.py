a8ad4afbd3cd0fb0fa54fdf206e62abed412a016
NxtaSunstrike <unixalle765@mail.ru>
2024-10-11 12:22:07 -0400

    inject postgres dependencies

--- src/app/infrastructure/dependency_injection/adapters/provide_sqla_db.py
+++ src/app/infrastructure/dependency_injection/adapters/provide_sqla_db.py
@@ -1,7 +1,9 @@
-from typing import Self
+from typing import Self, AsyncGenerator
+from sqlalchemy.ext.asyncio import AsyncEngine, async_sessionmaker, AsyncSession
 from dishka import Provider, Scope, provide
 
 from src.app.infrastructure.database.postgres.config import PostgresConfig
+from src.app.infrastructure.database.postgres.main import postgres_engine, postgres_session, postgres_session_factory
 
 
 class SqlaProvider(Provider):
@@ -9,4 +11,14 @@ class SqlaProvider(Provider):
     def provide_sqla_config(self: Self) -> PostgresConfig:
         return PostgresConfig
     
+    @provide(scope=Scope.APP)
+    async def provide_engine(self: Self, config: PostgresConfig) -> AsyncGenerator[AsyncEngine, None]:
+        return await postgres_engine(config = config)
     
+    @provide(scope=Scope.APP)
+    def provide_postgres_session_factory(self: Self, engine: AsyncEngine) -> async_sessionmaker[AsyncSession]:
+        return postgres_session_factory(engine = engine)
+    
+    @provide(scope=Scope.REQUEST)
+    async def provide_postgres_session(self: Self, session_factory: async_sessionmaker[AsyncSession]) -> AsyncGenerator[AsyncSession, None]:
+        return await postgres_session(session_factory = session_factory)
