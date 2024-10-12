21b1bbec0b636d6b58cfee62a70fc5108acceb48
NxtaSunstrike <unixalle765@mail.ru>
2024-10-12 10:59:39 -0400

    nats exception handlers

--- src/app/presentation/exceptions.py
+++ src/app/presentation/exceptions.py
@@ -1,5 +1,6 @@
 from fastapi import status, FastAPI
 from faststream.nats.annotations import NatsBroker
+from faststream import ExceptionMiddleware
 
 from src.app.domain.user.exceptions import UserAlreadyExistsError, UserNotFoundError
 from src.app.domain.common.exceptions import DomainValidationError
@@ -12,5 +13,12 @@ def fastapi_exceptions_factory(app: FastAPI) -> None:
     app.add_exception_handler(Exception, status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
-def nats_exceptions_factory(broker: ...) -> None:
-    ...
+def nats_exceptions_factory(broker: NatsBroker) -> None:
+    broker.add_middleware(ExceptionMiddleware(
+        handlers={
+            UserAlreadyExistsError: status.HTTP_409_CONFLICT,
+            UserNotFoundError: status.HTTP_404_NOT_FOUND,
+            DomainValidationError: status.HTTP_400_BAD_REQUEST,
+            Exception: status.HTTP_500_INTERNAL_SERVER_ERROR
+        }
+    ))
