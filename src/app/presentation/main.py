ec13b4f17ef3479675a799137f6b7bcbdcea0c9b
NxtaSunstrike <unixalle765@mail.ru>
2024-10-12 11:00:52 -0400

    including exceptions

--- src/app/presentation/main.py
+++ src/app/presentation/main.py
@@ -3,15 +3,18 @@ from faststream.nats.annotations import NatsBroker
 
 from src.app.presentation.api.controllers.nats import user_routers as nats_users, health_check_router as nats_health
 from src.app.presentation.api.controllers.http import user_routers as http_users, health_check_router as http_health
+from src.app.presentation.exceptions import nats_exceptions_factory
+from src.app.presentation.exceptions import fastapi_exceptions_factory
 
 
 def nats_controllers_factory(broker: NatsBroker) -> None:
     broker.include_router(nats_users.router)
     broker.include_router(nats_health.router)
-    broker.add_middleware(...)
+    nats_exceptions_factory(broker = broker)
+    
 
 
 def http_controllers_factory(app: FastAPI) -> None:
     app.include_router(http_users.router)
     app.include_router(http_health.router)
-    app.add_middleware(...)
+    fastapi_exceptions_factory(app = app)
