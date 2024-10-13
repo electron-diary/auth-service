f296e497895001baa099ccb4c1c93516f6c8a950
NxtaSunstrike <unixalle765@mail.ru>
2024-10-13 03:48:43 -0400

    alembic revisions

--- src/app/infrastructure/database/postgres/migrations/env.py
+++ src/app/infrastructure/database/postgres/migrations/env.py
@@ -5,6 +5,10 @@ from sqlalchemy import pool
 
 from alembic import context
 
+from src.app.infrastructure.database.postgres.config import PostgresConfig
+from src.app.infrastructure.database.postgres.models.declarative import Base
+from src.app.infrastructure.database.postgres.models.user import UserModel
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -14,11 +18,15 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
+config.set_main_option(
+    "sqlalchemy.url", PostgresConfig().postgres_url + '?async_fallback=True'
+)
+
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
