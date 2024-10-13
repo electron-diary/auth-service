e3ae942673fec4329d65727b48d2c8215120ab46
NxtaSunstrike <unixalle765@mail.ru>
2024-10-13 13:35:14 -0400

    editing engine postgres in infra

--- src/app/infrastructure/database/postgres/main.py
+++ src/app/infrastructure/database/postgres/main.py
@@ -9,10 +9,7 @@ def postgres_engine(config: PostgresConfig) -> AsyncEngine:
     engine: AsyncEngine = create_async_engine(
         url=config.postgres_url,
         echo=True,
-        future=True,
         echo_pool=config.echo,
-        json_serializer=lambda obj: json.dumps(obj, ensure_ascii=False),
-        json_deserializer=lambda json_str: json.loads(json_str, strict=False),
         pool_size=50
     )
     return engine
