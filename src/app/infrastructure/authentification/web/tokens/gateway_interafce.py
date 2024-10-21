458f168c879cec36bc4b39e406c5e2a2586726a4
NxtaSunstrike <unixalle765@mail.ru>
2024-10-21 11:49:45 -0400

    edit gateways && repositories

--- src/app/infrastructure/authentification/web/tokens/gateway_interafce.py
+++ src/app/infrastructure/authentification/web/tokens/gateway_interafce.py
@@ -1,28 +1,30 @@
 from typing import Self, Protocol
 from abc import abstractmethod
 
+from src.app.infrastructure.authentification.web.tokens.constants import TokenData
+from src.app.domain.value_objects.user_uuid_value_object import UserUUID
 
 class JwtGatewayInterface(Protocol):
     @abstractmethod
-    async def get_token(self: Self) -> ...:
+    async def get_tokens(self: Self, user_uuid: UserUUID) -> list[TokenData]:
         raise NotImplementedError(
             'method must be implemented by subclasses'
         )
     
     @abstractmethod
-    async def save_token(self: Self) -> None:
+    async def save_token(self: Self, token_data: TokenData) -> None:
         raise NotImplementedError(
             'method must be implemented by subclasses'
         )
     
     @abstractmethod
-    async def remove_token(self: Self) -> None:
+    async def remove_token(self: Self, user_uuid: UserUUID) -> None:
         raise NotImplementedError(
             'method must be implemented by subclasses'
         )
     
     @abstractmethod
-    async def update_token(self: Self) -> None:
+    async def update_token(self: Self, token_data: TokenData) -> None:
         raise NotImplementedError(
             'method must be implemented by subclasses'
         )
--- src/app/infrastructure/authentification/web/tokens/repository_interface.py
+++ src/app/infrastructure/authentification/web/tokens/repository_interface.py
@@ -1,28 +1,29 @@
 from typing import Protocol, Self
 from abc import abstractmethod
 
+from src.app.infrastructure.authentification.web.tokens.constants import UserData, TokenData
 
 class JwtRepositoryInterface(Protocol):
     @abstractmethod
-    def decode_token(self: Self, token: str | bytes) -> ...:
+    def decode_token(self: Self, token: str | bytes) -> UserData:
         raise NotImplementedError(
             'method must be implemented by subclasses'
         )
 
     @abstractmethod
-    def encode_access_token(self: Self) -> str | bytes:
+    def encode_access_token(self: Self, user_data: UserData) -> TokenData:
         raise NotImplementedError(
             'method must be implemented by subclasses'
         )
     
     @abstractmethod
-    def encode_refresh_token(self: Self) -> str | bytes:
+    def encode_refresh_token(self: Self, user_data: UserData) -> TokenData:
         raise NotImplementedError(
             'method must be implemented by subclasses'
         )
     
     @abstractmethod
-    def update_access_token(self: Self, refresh_token: str | bytes) -> str | bytes:
+    def update_access_token(self: Self, refresh_token: TokenData) -> TokenData:
         raise NotImplementedError(
             'method must be implemented by subclasses'
         )
