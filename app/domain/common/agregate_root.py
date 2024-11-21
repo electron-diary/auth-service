3e350782c5ffbc8d6d7332fad9fcb6d704e162cb
NxtaSunstrike <unixalle765@mail.ru>
2024-11-21 13:22:46 -0500

    add domain event anf agregate root

---
+++ app/domain/common/agregate_root.py
@@ -0,0 +1,17 @@
+from typing import Self
+from dataclasses import dataclass, field
+
+from app.domain.common.common_domain_event import CommonDomainEvent
+
+
+@dataclass
+class AgregateRoot:
+    _events: list[CommonDomainEvent] = field(default_factory=list, init=False)
+
+    def add_event(self: Self, event: ...) -> None:
+        self._events.append(event)
+
+    def push_events(self: Self) -> list[CommonDomainEvent]:
+        events: list[CommonDomainEvent] = self._events.copy()
+        self._events.clear()
+        return events
---
+++ app/domain/common/common_domain_event.py
@@ -0,0 +1,9 @@
+from dataclasses import dataclass, field
+from uuid import UUID, uuid4
+from datetime import datetime
+
+
+@dataclass(frozen=True)
+class CommonDomainEvent:
+    event_uuid: UUID  = field(default_factory=uuid4, init=False)
+    event_timestamp: datetime = field(default_factory=datetime.now, init=False)
