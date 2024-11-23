26f56b2bd5cf2104b7d8b3a7d7d358bea1eb7226
NxtaSunstrike <unixalle765@mail.ru>
2024-11-23 11:58:28 -0500

    remove frozen=True from CommonDomainEntity

--- app/domain/common/common_domain_entity.py
+++ app/domain/common/common_domain_entity.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 from app.domain.common.common_domain_value_object import CommonDomainValueObject
 
 
-@dataclass(frozen=True)
+@dataclass
 class CommonDomainEntity[EntityId: CommonDomainValueObject]:
     id: EntityId
 
