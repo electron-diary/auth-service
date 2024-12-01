07ef454364c070c7e0d93fe5937207dfaaa2476c
NxtaSunstrike <unixalle765@mail.ru>
2024-12-01 13:27:30 -0500

    edit base application exception

--- app/application/base/exception.py
+++ app/application/base/exception.py
@@ -1,6 +1,5 @@
-from typing import Self
+from dataclasses import dataclass
 
-
+@dataclass
 class ApplicationError(Exception):
-    def __init__(self: Self, message: str) -> None:
-        super().__init__(message)
+    ...
