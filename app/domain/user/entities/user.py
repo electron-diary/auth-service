b6cfa604d06d49d2a0a285f1c1e39c3f5321f6e3
NxtaSunstrike <unixalle765@mail.ru>
2024-12-09 14:57:48 -0500

    edit user entity

--- app/domain/user/entities/user.py
+++ app/domain/user/entities/user.py
@@ -10,11 +10,11 @@ class User(UowedEntity[None]):
         self: Self,
         uow: UnitOfWorkInterface,
         id: ...,
-        profile: Profile,
+        profiles: list[Profile],
     ) -> None:
         super().__init__(uow=uow, id=id)
 
-        self.profile: Profile = profile
+        self.profile: list[Profile] = profiles
 
     @classmethod
     def create(cls: type[Self], uow: UnitOfWorkInterface) -> Self:
@@ -22,25 +22,47 @@ class User(UowedEntity[None]):
         user.mark_new()
 
         return user
+    
+    def add_profile(self: Self) -> None:
+        profile = Profile.create(uow=self.uow)
+        self.profile.append(profile)
+
+    def delete_profile(self: Self) -> None:
+        for profile in self.profile:
+            if profile.id == ...:
+                profile.delete()
 
     def add_avatar(self: Self) -> None:
-        self.profile.add_avatar()
+        for profile in self.profile:
+            if profile.id == ...:
+                profile.add_avatar()
 
     def delete_avatar(self: Self) -> None:
-        self.profile.delete_avatar()
+        for profile in self.profile:
+            if profile.id == ...:
+                profile.delete_avatar()
 
     def add_social_network(self: Self) -> None:
-        self.profile.add_social_network()
+        for profile in self.profile:
+            if profile.id == ...:
+                profile.add_social_network()
 
     def delete_social_network(self: Self) -> None:
-        self.profile.delete_social_network()
+        for profile in self.profile:
+            if profile.id == ...:
+                profile.delete_social_network()
 
     def add_address(self: Self) -> None:
-        self.profile.add_address()
+        for profile in self.profile:
+            if profile.id == ...:
+                profile.add_address()
 
     def delete_address(self: Self) -> None:
-        self.profile.delete_address()
+        for profile in self.profile:
+            if profile.id == ...:
+                profile.delete_address()
 
     def delete(self: Self) -> None:
-        self.profile.delete()
         self.mark_deleted()
+        for profile in self.profile:
+            profile.delete()
